413e270dbe4785a936673ac75bf960586cc98336
David <yoxisem544@gmail.com>
2017-03-20 12:43:46 +0800

    setup project

--- SwiftGirlsAPI/ViewController.swift
+++ SwiftGirlsAPI/ViewController.swift
@@ -8,18 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final public class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var displayingLabel: UILabel!
+    
+    public override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
